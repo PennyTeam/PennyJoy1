401a124f506df37bcff5ff7dadb5fda69db7675c
McNester <seraf5@bk.ru>
2021-04-18 19:19:47 +0600

    добавил проверку на большие буквы

--- app/src/main/java/com/example/pennyjoy/SignUpActivityNoAcc1.java
+++ app/src/main/java/com/example/pennyjoy/SignUpActivityNoAcc1.java
@@ -24,13 +24,15 @@ public class SignUpActivityNoAcc1 extends AppCompatActivity {
     //здесь он клик для вызова след активити регистрации
     public void nextLog1(View v){
         //проверка на пустоту данных ввода
-        if( !txtName.getText().toString().isEmpty() && !txtSurname.getText().toString().isEmpty()
-        ) {
-            Intent intent = new Intent(this, SignUpActivityNoAcc2.class);
-            intent.putExtra("name",txtName.getText().toString());
-            intent.putExtra("surname",txtSurname.getText().toString());
-
-            startActivityForResult(intent, signUpNoAccRequestCode);
+        if( !txtName.getText().toString().isEmpty() && !txtSurname.getText().toString().isEmpty()) {
+            String name= txtName.getText().toString().charAt(0)+"";
+            String surname= txtSurname.getText().toString().charAt(0)+"";
+            if(name.matches("А-Я") && name.matches("А-Я") ) {
+                Intent intent = new Intent(this, SignUpActivityNoAcc2.class);
+                intent.putExtra("name", txtName.getText().toString());
+                intent.putExtra("surname", txtSurname.getText().toString());
+                startActivityForResult(intent, signUpNoAccRequestCode);
+            }
         }else{
             Toast.makeText(getApplicationContext(),"Заполните все поля верно",Toast.LENGTH_LONG).show();
         }
