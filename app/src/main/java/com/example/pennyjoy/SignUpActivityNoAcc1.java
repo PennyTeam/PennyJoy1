2015c14f523c81bc756fabf1f720766c8e2dd8e1
McNester <seraf5@bk.ru>
2021-04-18 19:54:11 +0600

    добавил проверку на большие буквы 5

--- app/src/main/java/com/example/pennyjoy/SignUpActivityNoAcc1.java
+++ app/src/main/java/com/example/pennyjoy/SignUpActivityNoAcc1.java
@@ -25,16 +25,29 @@ public class SignUpActivityNoAcc1 extends AppCompatActivity {
     public void nextLog1(View v){
         //проверка на пустоту данных ввода
         if( !txtName.getText().toString().isEmpty() && !txtSurname.getText().toString().isEmpty()) {
+            boolean nameIsUpper=false;
+            boolean surnameIsUpper=false;
+
+            //здесь проверки на большие первые буквы
             if( Character.isUpperCase(txtName.getText().toString().charAt(0)) ) {
-                if(Character.isUpperCase(txtSurname.getText().toString().charAt(0))) {
-                    Intent intent = new Intent(this, SignUpActivityNoAcc2.class);
-                    intent.putExtra("name", txtName.getText().toString());
-                    intent.putExtra("surname", txtSurname.getText().toString());
-                    startActivityForResult(intent, signUpNoAccRequestCode);
-                }else{ txtSurname.setError("Введите фамилию корректно");
-                }
+                nameIsUpper=true;
+
             }else{
                 txtName.setError("Введите имя корректно");
+            }
+            if(Character.isUpperCase(txtSurname.getText().toString().charAt(0))) {
+                surnameIsUpper=true;
+
+            }else{
+                txtSurname.setError("Введите фамилию корректно");
+            }
+
+            //здесь проверяем те бул переменные; отвечающие за наличие больших первых букв
+            if(nameIsUpper == true && surnameIsUpper == true){
+                Intent intent = new Intent(this, SignUpActivityNoAcc2.class);
+                intent.putExtra("name", txtName.getText().toString());
+                intent.putExtra("surname", txtSurname.getText().toString());
+                startActivityForResult(intent, signUpNoAccRequestCode);
             }
         }else{
             Toast.makeText(getApplicationContext(),"Заполните все поля",Toast.LENGTH_LONG).show();
