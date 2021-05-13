86280246d8d9c9a392da9d3639a45506503daf7c
McNester <seraf5@bk.ru>
2021-05-13 10:47:56 +0600

    some design of goal

--- app/src/main/java/com/example/pennyjoy/MoneyPigActivity.java
+++ app/src/main/java/com/example/pennyjoy/MoneyPigActivity.java
@@ -2,7 +2,9 @@ package com.example.pennyjoy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MoneyPigActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MoneyPigActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_money_pig);
+    }
+
+    public void addGoalClicked(View v){
+        Intent intent=new Intent(this,AddGoalActivity.class);
+        startActivity(intent);
     }
 }
