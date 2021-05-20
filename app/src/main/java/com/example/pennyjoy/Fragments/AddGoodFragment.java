367511d3766b85f0a8c5d5425218c0a99c227a43
Kiri11i <ksotn2003@gmail.com>
2021-05-20 22:46:50 +0300

    h3eff44444444444444444444444

--- app/src/main/java/com/example/pennyjoy/Fragments/AddGoodFragment.java
+++ app/src/main/java/com/example/pennyjoy/Fragments/AddGoodFragment.java
@@ -66,6 +66,7 @@ public class AddGoodFragment extends Fragment {
 
     //Is there anybody out there that's payin' attention?
     //i'm here!1!!1!
+    //grgrgrgrhrhrhththtttttttttttttttttttttt
 
     @Nullable
     @Override
