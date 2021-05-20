689ff812d4d8886739b6c8e78a05faac203c0f80
McNester <seraf5@bk.ru>
2021-05-20 23:28:04 +0600

    PLEASE

--- app/src/main/java/com/example/pennyjoy/ChartsActivity.java
+++ app/src/main/java/com/example/pennyjoy/ChartsActivity.java
@@ -27,6 +27,8 @@ public class ChartsActivity extends AppCompatActivity {
         //hvjv
 
         //gohg
+
+
         PieChart pieChart = findViewById(R.id.pieChartOfSpendings);
 
         ArrayList<PieEntry> students = new ArrayList<PieEntry>();
